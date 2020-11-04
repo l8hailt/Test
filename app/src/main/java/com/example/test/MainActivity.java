7e7a5f30c11e56d5d2be0372fea11b56e354e65d
Le Thanh Hai <l8hailt@gmail.com>
2020-11-04 22:12:17 +0700

    - test javascriptInterface

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,19 +3,37 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
-        TestFragment fragment = TestFragment.getInstance();
+//        TestFragment fragment = TestFragment.getInstance();
+//
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .add(R.id.mainLayout, fragment, "TestFragment")
+//                .commitAllowingStateLoss();
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.mainLayout, fragment, "TestFragment")
-                .commitAllowingStateLoss();
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.setWebViewClient(new WebViewClient());
+        webView.addJavascriptInterface(this, "JSAction");
+
+        webView.loadUrl("file:///android_asset/test.html");
+
+    }
+
+    @JavascriptInterface
+    public void action(int number) {
+        Log.e("TAG", "action: " + number);
     }
 }
